b31fde65f8961833f5549eab19ec1ff9b4340600
기성환 <kish1919@gmail.com>
2022-07-19 22:54:09 -0500

    initial commit

---
+++ src/Comment.java
@@ -0,0 +1,11 @@
+public class Comment {
+    public static void main(String[] args) {
+        // 주석을 배워봅시다.
+        System.out.println("주석을 배워봅시다");
+
+        /* d이것은 여러줄의 주석입니다
+        두번째 줄입니다.
+        세번째 줄입니다.
+         */
+    }
+}
